bdcc4e6a67077614ad3e13e951984af4abff8354
Martin Grotzke <martin.grotzke@googlemail.com>
2014-03-20 02:08:05 +0100

    Update datastax to 2.0.1, fixes wrong dependency in maven central.
    The build.gradle depended on the cassandra-driver-core 1.0.0 which
    broke my application with a
      `ClassNotFoundException: com.datastax.driver.core.Query`
    
    A bit weird is that the pom.xml (at http://repo1.maven.org/maven2/com/streamsend/pillar/1.0.2/pillar-1.0.2.pomhttp://repo1.maven.org/maven2/com/streamsend/pillar/1.0.2/pillar-1.0.2.pom) specifies 1.0.2 - no clue where this is coming
    from. Still, the 1.x version seems wrong.

--- project/PillarBuild.scala
+++ project/PillarBuild.scala
@@ -14,7 +14,7 @@ object PillarBuild extends Build {
   }
 
   val dependencies = Seq(
-    "com.datastax.cassandra" % "cassandra-driver-core" % "2.0.0",
+    "com.datastax.cassandra" % "cassandra-driver-core" % "2.0.1",
     "com.typesafe" % "config" % "1.0.1",
     "org.clapper" %% "argot" % "1.0.1",
     "org.mockito" % "mockito-core" % "1.9.5" % "test",

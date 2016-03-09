4ccd5876b023f03bb2025903e04dd51bb5bae7be
Marco Priebe <marco.priebe@inoio.de>
2016-03-09 15:45:15 +0100

    upgrade to Cassandra driver 3.0.0

--- project/PillarBuild.scala
+++ project/PillarBuild.scala
@@ -15,7 +15,7 @@ object PillarBuild extends Build {
   }
 
   val dependencies = Seq(
-    "com.datastax.cassandra" % "cassandra-driver-core" % "2.0.2",
+    "com.datastax.cassandra" % "cassandra-driver-core" % "3.0.0",
     "com.typesafe" % "config" % "1.0.1",
     "org.clapper" %% "argot" % "1.0.3",
     "org.mockito" % "mockito-core" % "1.9.5" % "test",
